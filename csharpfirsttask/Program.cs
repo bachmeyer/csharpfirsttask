eff72aeb9c682259648ed32876516cc0769821ab
bachmeyer <wesyb@yahoo.com>
2016-01-20 13:32:58 -0600

    initial upload of basic c sharp tasks

---
+++ csharpfirsttask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpfirsttask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pulls in the date from the user and displays the date in long format
+            //Console.Write("Enter the date: ");
+            //var enteredDate = DateTime.Parse(Console.ReadLine());
+            //Console.WriteLine(enteredDate.ToString("D"));
+            //Console.ReadLine();
+
+            var crazyText = "He said \"Awesome!\" \nBut I thought he was crazy.";
+            Console.WriteLine(crazyText);
+        }
+    }
+}
